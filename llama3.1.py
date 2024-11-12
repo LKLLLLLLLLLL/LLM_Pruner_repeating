6805ab38c486ec96a2824a8cc247aac0c0fc4512
auto-dl_user <13263311018@163.com>
2024-11-12 17:09:03 +0800

    更新了`README`条目，添加了`llama3.1.py`，现在可以实现部署llama3.1模型的功能

---
+++ llama3.1.py
@@ -0,0 +1,45 @@
+# 此文件使用transformers库，下载并部署llama3.1-8B-instruct模型，可以在终端中运行，实现对话生成功能
+import transformers
+import torch
+
+model_id = "meta-llama/Meta-Llama-3.1-8B-Instruct"
+cache_dir = "/root/autodl-tmp/cache" # 模型缓存目录，别的文件也可以直接使用该缓存
+
+#登录hugging-face
+from huggingface_hub import login
+login() 
+
+# 加载 tokenizer
+tokenizer = transformers.AutoTokenizer.from_pretrained(model_id, cache_dir=cache_dir)
+
+# 加载模型
+model = transformers.AutoModelForCausalLM.from_pretrained(model_id, torch_dtype=torch.bfloat16, cache_dir=cache_dir)
+
+# 将模型移动到 GPU
+model.to("cuda:0")
+
+# 创建 pipeline
+pipeline = transformers.pipeline(
+    "text-generation",
+    model=model,
+    tokenizer=tokenizer,
+    device="cuda:0",  # 指定设备为第一个 GPU
+    pad_token_id=tokenizer.eos_token_id
+)
+
+# 提示词
+messages = [
+    {"role": "system", "content": "You are a chatbot who do what you are told to do."},
+]
+
+# 连续对话
+while True:
+    user_input = input("user: ")
+    messages.append({"role": "user", "content": user_input})
+    outputs = pipeline(
+        messages,
+        max_new_tokens=1024,
+    )
+    answer = outputs[0]["generated_text"][-1]["content"]
+    messages.append({"role": "chatbot", "content": answer})
+    print(f"\nllama: {answer}\n")
